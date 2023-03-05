ece1f845cce595a0673005215fcb3370b21828d1
areeb786 <areebrockzz786@gmail.com>
2023-03-05 19:20:38 +0530

    Initial commit

---
+++ app/src/main/java/com/areeb/fakejsoon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.areeb.fakejsoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
